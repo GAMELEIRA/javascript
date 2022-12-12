be05ab1d25683c1024316cc1012f4733044838f2
GAMELEIRA <gameleira270499@gmail.com>
2022-12-11 23:11:56 -0300

    Adiciona estudo sobre funcoes de array

---
+++ javascript-typescript/javascript-basico/arrays.js
@@ -0,0 +1,21 @@
+const alunos = ['Luiz', 'Maria', 'João'];
+
+console.log(alunos instanceof Array);
+
+console.log(alunos);
+console.log(alunos[0]);
+console.log(alunos[1]);
+console.log(alunos[2]);
+
+console.log(alunos.length); //TAMANHO DO ARRAY
+alunos.push('Gabriel'); //ADICIONA NO FINAL DA FILA
+console.log(alunos);
+
+alunos.unshift('Cibele'); //ADICIONA NO COMECO DA FILA
+alunos.pop(); //REMOVE DO FINAL
+alunos.shift(); //ALTERA DO COMECO
+
+delete alunos[1]; //DELETA VALOR DA POSICAO PASSADA PELO PARAMETRO
+
+alunos.slice(0, 3); //FATIA UM PEDACO DO ARRAY COM BASE NA POSICAO INICIAL E POSICAO FINAL QUE ANTECEDE O VALOR PASSADO COMO PARAMETRO.  
+alunos.slice(0. -2); //FATIA AS ULTIMAS POSICOES DO ARRAY
